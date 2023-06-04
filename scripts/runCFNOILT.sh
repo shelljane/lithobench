a7bc00d0edb8933cc6f1c3b08d0d29eabdcc25b8
Su Zheng <szheng22@cse.cuhk.edu.hk>
2023-06-05 05:26:05 +0800

    Subtle changes.

--- scripts/runCFNOILT.sh
+++ scripts/runCFNOILT.sh
@@ -3,7 +3,7 @@
 python3 lithobench/train.py -m CFNOILT -s MetalSet -n 8 -b 4 -p False
 # python3 lithobench/train.py -m CFNOILT -s ViaSet -n 2 -b 4 -p False
 
-# python3 lithobench/test.py -m CFNOILT -s MetalSet -l saved/MetalSet_CFNOILT/net.pth
-# python3 lithobench/test.py -m CFNOILT -s ViaSet -l saved/ViaSet_CFNOILT/net.pth
-# python3 lithobench/test.py -m CFNOILT -s StdMetal -l saved/MetalSet_CFNOILT/net.pth
-# python3 lithobench/test.py -m CFNOILT -s StdContact -n 16 -b 4 -l saved/ViaSet_CFNOILT/net.pth
+# python3 lithobench/test.py -m CFNOILT -s MetalSet -l saved/MetalSet_CFNOILT/net.pth --shots
+# python3 lithobench/test.py -m CFNOILT -s ViaSet -l saved/ViaSet_CFNOILT/net.pth --shots
+# python3 lithobench/test.py -m CFNOILT -s StdMetal -l saved/MetalSet_CFNOILT/net.pth --shots
+# python3 lithobench/test.py -m CFNOILT -s StdContact -n 16 -b 4 -l saved/ViaSet_CFNOILT/net.pth --shots
--- scripts/runNeuralILT.sh
+++ scripts/runNeuralILT.sh
@@ -3,7 +3,7 @@
 python3 lithobench/train.py -m NeuralILT -s MetalSet -n 8 -b 12 -p True
 # python3 lithobench/train.py -m NeuralILT -s ViaSet -n 2 -b 12 -p True
 
-# python3 lithobench/test.py -m NeuralILT -s MetalSet -l saved/MetalSet_NeuralILT/net.pth
-# python3 lithobench/test.py -m NeuralILT -s ViaSet -l saved/ViaSet_NeuralILT/net.pth
-# python3 lithobench/test.py -m NeuralILT -s StdMetal -l saved/MetalSet_NeuralILT/net.pth
-# python3 lithobench/test.py -m NeuralILT -s StdContact -n 16 -b 12 -l saved/ViaSet_NeuralILT/net.pth
+# python3 lithobench/test.py -m NeuralILT -s MetalSet -l saved/MetalSet_NeuralILT/net.pth --shots
+# python3 lithobench/test.py -m NeuralILT -s ViaSet -l saved/ViaSet_NeuralILT/net.pth --shots
+# python3 lithobench/test.py -m NeuralILT -s StdMetal -l saved/MetalSet_NeuralILT/net.pth --shots
+# python3 lithobench/test.py -m NeuralILT -s StdContact -n 16 -b 12 -l saved/ViaSet_NeuralILT/net.pth --shots
